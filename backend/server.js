23fa7649cc436780842657507b816afe1df9d73d
pmin825 <pmin.0825@gmail.com>
2021-04-27 14:47:48 -0700

    update server file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ const app = express();
 const path = require("path");
 require("dotenv").config();
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    app.get("/", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "frontend", "build", "index.html")
+        );
+    });
+}
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     next();
@@ -73,15 +82,6 @@ app.get("/:id", async (req, res) => {
         res.status(404).json(error.message);
     }
 });
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
-    app.get("/", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, "frontend", "build", "index.html")
-        );
-    });
-}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
